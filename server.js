afd09302c5de24ef43eac33e7843e323a735efc4
boulid <test.boulid@gmail.com>
2017-08-05 16:58:18 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,52 @@
 var express = require('express');
 var cors = require('cors');
+var jsonfile = require('jsonfile');
+var bodyParser = require('body-parser')
 
 var app = express();
 
+var file = './data.json';
 
 // your manifest must have appropriate CORS headers, you could also use '*'
 app.use(cors({ origin: '*' }));
 //app.use(cors({ origin: 'https://trello.com' }));
 
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
+
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-app.use(express.static('view'));
+app.use(express.static('views'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("*", function (request, response) {
+/*app.get("*", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
+});*/
+
+app.get("/data", function (request, response) {
+    response.setHeader('Content-Type', 'application/json');
+    response.sendFile(__dirname + '/data.json');
+//    
+//    jsonfile.readFile(__dirname + '/data.json', function (err, obj) {
+//        response.send(JSON.stringify(obj));
+//    });
 });
 
-process.env.PORT=3000;
+app.post('/', function (sReq, sRes) {
+    console.dir(sReq.body);
+    jsonfile.writeFile(file, sReq.body, { flag: 'a' }, function (err) {
+        console.log(err);
+    });
+});
+
+//var obj = { _user: userName, _tentations: tentations, _items: selecteditems };
+
 
 // listen for requests :)
-var listener = app.listen(3000, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   //console.log(__dirname + '/views/index.html');
   console.log('Your app is listening on port ' + listener.address().port);
 });
